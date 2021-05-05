94779e89b4b5611e282dd908b46ccf3b2ae3edf9
MoAI <urotanre522@gmail.com>
2021-05-05 14:52:21 +0900

    修正

--- pages/users/[user_name].tsx
+++ pages/users/[user_name].tsx
@@ -1,27 +1,51 @@
-import { useRouter } from "next/router";
 import Head from "next/head";
-import React, { useState } from "react";
-import { NextPage } from "next";
+import React from "react";
+import { GetServerSideProps, NextPage } from "next";
 
-const UserPage: NextPage = () => {
-  const router = useRouter();
-  const { user_name } = router.query;
-  const [userDescription] = useState<string>(
-    "自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介"
-  );
-
+interface IUserPageSSProps {
+  URL: string;
+  userName: string;
+  userDescription: string;
+  imageSrc: string;
+}
+const UserPage: NextPage<IUserPageSSProps> = ({
+  URL,
+  userName,
+  userDescription,
+  imageSrc,
+}) => {
   return (
     <div>
       <Head>
         <title>OGP TEST-USER PAGE</title>
-        <meta property="og:url" content={router.asPath} />
-        <meta property="og:title" content={`user_name:${user_name}`} />
+        <meta property="og:url" content={URL} />
+        <meta property="og:title" content={`user_name:${userName}`} />
         <meta property="og:description" content={userDescription} />
         <meta property="og:site_name" content="OGP TEST" />
-        <meta property="og:image" content="/images/ogp-thumbnail.jpg" />
+        <meta property="og:image" content={imageSrc} />
       </Head>
-      <p>user_name: {user_name}</p>
+      <p>user_name: {userName}</p>
+      <p>URL: {URL}</p>
+      <p>userDescription: {userDescription}</p>
+      <img src={imageSrc} />
     </div>
   );
 };
 export default UserPage;
+
+export const getServerSideProps: GetServerSideProps<IUserPageSSProps> = async (
+  context
+) => {
+  return {
+    props: {
+      URL: context.resolvedUrl,
+      userName:
+        typeof context.query.user_name === "string"
+          ? context.query.user_name
+          : "",
+      userDescription:
+        "自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介",
+      imageSrc: "/images/ogp-thumbnail.jpg",
+    },
+  };
+};
